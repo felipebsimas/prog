9d7517cbe8d6bb7f4326b93f4879554b66d7f916
felipebsimas <48572016+felipebsimas@users.noreply.github.com>
2019-07-05 20:50:13 -0300

    Delete ex01.php

--- 2info1/ex01.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    print "Digite a temperatura em Fahrenheit" ;
-    $temperatura = (int) fgets(STDIN);
-
-    $tempgraus = ($temperatura - 32) * 5/9;
-
-    print "A temperatura em Celsius é $tempgraus";
-
